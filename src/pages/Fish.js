309fab3e03ded9354306b6ae2f21ef872e206a47
Marko Novakovic <marfy87@gmail.com>
2024-03-01 15:22:43 -0500

    Single page card spacing

--- src/pages/Fish.js
+++ src/pages/Fish.js
@@ -14,18 +14,20 @@ import { GET_FISH } from '../queries/fishQueries'
      <>
      {!loading && !error && (
         <div className='single-fish-page'>
-            <img src={data.getFish.imageUrl} alt={data.getFish.name} />
-            <div className='description'>
-                <h1>{data.getFish.name}</h1>
-                <p><strong>Family: </strong>{data.getFish.family.name}</p>
-                <p><strong>Care Level: </strong>{data.getFish.careLevel}</p>
-                <p><strong>Temperament: </strong>{data.getFish.temperament}</p>
-                <p><strong>Diet: </strong>{data.getFish.diet}</p>
-                <p><strong>Reef Compatible: </strong>{data.getFish.reefCompatibility ? 'Yes' : 'No'}</p>
-                <p><strong>Max Fish Size: </strong>{data.getFish.maxSize}</p>
-                <p><strong>Minimum Tank Size: </strong>{data.getFish.minTankSize}</p>
-                <br />
-                <Link to='/' className='btn'>Go Home</Link>
+            <div className='container'>
+                <img src={data.getFish.imageUrl} alt={data.getFish.name} />
+                <div className='description'>
+                    <h1>{data.getFish.name}</h1>
+                    <p><strong>Family: </strong>{data.getFish.family.name}</p>
+                    <p><strong>Care Level: </strong>{data.getFish.careLevel}</p>
+                    <p><strong>Temperament: </strong>{data.getFish.temperament}</p>
+                    <p><strong>Diet: </strong>{data.getFish.diet}</p>
+                    <p><strong>Reef Compatible: </strong>{data.getFish.reefCompatibility ? 'Yes' : 'No'}</p>
+                    <p><strong>Max Fish Size: </strong>{data.getFish.maxSize}</p>
+                    <p><strong>Minimum Tank Size: </strong>{data.getFish.minTankSize}</p>
+                    <br />
+                    <Link to='/' className='btn'>Go Home</Link>
+                </div>
             </div>
         </div>
      )}

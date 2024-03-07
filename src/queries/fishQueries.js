02de68aa5a6073983f235b24dce5a841f0f432b3
Marko Novakovic <marfy87@gmail.com>
2024-03-07 11:21:46 -0500

    Query string adjusted

--- src/queries/fishQueries.js
+++ src/queries/fishQueries.js
@@ -1,32 +1,33 @@
-import {gql} from '@apollo/client';
+import { gql } from "@apollo/client";
 
 const GET_FISHES = gql`
-query GetFishes {
-  getFishes {
-    id
-    name
-    imageUrl
-  }
-}`
-
-const GET_FISH = gql`
-query GetFish($id: ID) {
-  getFish(ID: $id) {
-    id
-    name
-    family {
+  query GetFishes {
+    getFishes {
       id
       name
+      imageUrl
     }
-    careLevel
-    temperament
-    diet
-    reefCompatibility
-    maxSize
-    minTankSize
-    imageUrl
   }
-}`
+`;
 
+const GET_FISH = gql`
+  query GetFish($id: ID) {
+    getFish(ID: $id) {
+      id
+      name
+      family {
+        id
+        name
+      }
+      careLevel
+      temperament
+      diet
+      reefCompatibility
+      maxSize
+      minTankSize
+      imageUrl
+    }
+  }
+`;
 
-export {GET_FISHES, GET_FISH}
+export { GET_FISHES, GET_FISH };
